bf598a9a615f0c58189af6406619e053263ebe66
nevelate <isfandiyorakbarov450@gmail.com>
2024-11-04 20:31:26 +0500

    DrawingView initial UI

---
+++ SimpleSketch/Views/DrawingView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SimpleSketch.Views;
+
+public partial class DrawingView : UserControl
+{
+    public DrawingView()
+    {
+        InitializeComponent();
+    }
+}
